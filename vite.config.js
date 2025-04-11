e38fba1756876b5c2f44006241e5bad52721ba2b
MarinaKorn <gecagem840@gmail.com>
2025-04-12 00:38:24 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
@@ -12,38 +13,24 @@ export default defineConfig(({ command }) => {
     root: 'src',
     base: '/goit-js-hw-10/', 
     build: {
+      outDir: '../dist',
+      emptyOutDir: true,
       sourcemap: true,
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
+            if (id.includes('node_modules')) return 'vendor';
           },
-          entryFileNames: chunkInfo => {
-            if (chunkInfo.name === 'commonHelpers') {
-              return 'commonHelpers.js';
-            }
-            return '[name].js';
-          },
-          assetFileNames: assetInfo => {
-            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
-              return '[name].[ext]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({
-        sort: 'mobile-first',
-      }),
+      FullReload(['./src/**/*.html']),
+      SortCss({ sort: 'mobile-first' }),
     ],
   };
 });
